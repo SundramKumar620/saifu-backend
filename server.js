0e20d04f1d0478d69e52b97878957479a0fe2eb6
Sundram Kumar <sk.sundramgupta521@gmail.com>
2026-02-08 00:23:47 +0530

    feat: Allow 'solana-client' header in CORS configuration.

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization','solana-client']
 }));
 
 // Rate limiting - prevent abuse
